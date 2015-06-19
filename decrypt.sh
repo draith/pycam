e6abf8cc939a982f4bc694aff0d85a11fd77336e
Douglas Raith <draith2001@gmail.com>
2015-06-19 23:02:44 +0100

    Use node-rsa for command decryption.

--- decrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Input arg = base64-encoded ssl-encrypted command.  
-# Result: decrypted command in plaintext file.  Using private key in key.pem
-# First, split the base64 into 64-char lengths.
-echo ${1:0:64} > base64.txt
-echo ${1:64} >> base64.txt
-# Convert to binary encrypted string.
-openssl enc -in base64.txt -out bintext -d -a
-openssl rsautl -decrypt -in bintext -out plaintext -inkey key.pem
--- runner.js
+++ runner.js
@@ -5,10 +5,11 @@ var child_process = require('child_process');
 var exec = child_process.exec;
 var path = "/home/pi/pycam/";
 var lastNumber = parseInt(fs.readFileSync(path + 'lastNum.txt','utf8'),10);
-
+var NodeRSA = require('node-rsa');
+var key = new NodeRSA(fs.readFileSync(path + 'key.pem','utf8'));
 function onRequest(request, response)
 {
-	// Display current status and link to start or stop.
+	// Display current status - running if pgrep returns non-empty string
 	function showPage(error, stdout, sterr)
 	{
 	exec("pgrep pycam.py", { timeout : 500 }, 
@@ -22,50 +23,42 @@ function onRequest(request, response)
 		});
 	}
 	// First, decrypt the encrypted command encoded in the url path
-	var scriptcmd = path + "decrypt.sh " + request.url.substring(1);
-	exec(scriptcmd, { timeout : 200 },
-	function (error, stdout, stderr) {
-		// Decrypted command should consist of <command>/<number>
-		var command = fs.readFileSync(path + 'plaintext','utf8');
-		var components = command.split('/');
-		if (components.length == 2)
+	var encrypted = request.url.substring(1);
+	var decrypted = key.decrypt(encrypted, 'utf8');
+	// Then split into command and number
+	var components = decrypted.split('/');
+	if (components.length == 2)
+	{
+		command = components[0];
+		if (command == "Status" || command == "Switch")
 		{
-			// Check that number > last number : prevent repeat attacks
-			// console.log('command = ' + components[0]);
 			var newNumber = parseInt(components[1],10);
-			// console.log('newNumber = ' + newNumber);
-			if (newNumber <= lastNumber)
+			// Check that newNumber > lastNumber : prevent repeat attacks
+			if (command == "Switch" && newNumber > lastNumber)
 			{
-				console.log('newNumber is not greater than lastNumber ' + lastNumber);
+				exec("pgrep pycam.py", { timeout : 500 }, 
+				function (error, stdout, sterr) {
+					var running = (stdout != '');
+					// Start or Stop before displaying the updated state.
+					command = "sudo /etc/init.d/pycam " + (running ? 'stop' : 'start');
+					exec(command, { timeout: 500 },
+						showPage
+						);
+				});
 			}
 			else
 			{
+				// Just display the current state.
+				showPage();
+			}
+			if (newNumber > lastNumber)
+			{
+				// Save new number
+				fs.writeFileSync(path + "lastNum.txt", newNumber.toString());
 				lastNumber = newNumber;
-				command = components[0];
-				if (command == "Switch")
-				{
-					// Save new number
-					fs.writeFileSync(path + "lastNum.txt", newNumber.toString());
-					exec("pgrep pycam.py", { timeout : 500 }, 
-					function (error, stdout, sterr) {
-						var running = (stdout != '');
-						// Start or Stop before displaying the updated state.
-						command = "sudo /etc/init.d/pycam " + (running ? 'stop' : 'start');
-						exec(command, { timeout: 500 },
-							showPage
-							);
-					});
-				}
-				else if (command == "Status")
-				{
-					// Save new number
-					fs.writeFileSync(path + "lastNum.txt", newNumber.toString());
-					// Just display the current state.
-					showPage();
-				}
 			}
 		}
-	});
+	}
 } // onRequest
 
 http.createServer(onRequest).listen(8998);
