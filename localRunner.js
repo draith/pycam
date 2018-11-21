f5457e625727c09674adc046375743d71046163a
Douglas Raith <draith2001@gmail.com>
2018-11-21 22:28:52 +0000

    Redirected on switch to main page.

--- localRunner.js
+++ localRunner.js
@@ -7,24 +7,12 @@ var url = require("url");
 
 function onRequest(request, response)
 {
-  function writePage()
+  function reloadEntryPage(error, stdout, sterr)
   {
-    // Display current status - running if pgrep returns non-empty string
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write('<html><head><title>Picam Local Control</title></head><body style="font-size:10vw;"><h1>Picam</h1><h1 style = "color:');
-  	exec("pgrep pycam.py", { timeout : 500 },
-  		function (error, stdout, sterr) {
-  			var running = (stdout != '');
-  			response.write(running ? 'green">Running' : 'red">Stopped');
-        response.write("</h1>");
-        var command = (running ? 'Stop' : 'Start');
-        response.write('<h1><a href="/' + command + '">' + command + '</a></h1>');
-        response.end('</body></html>');
-  		});
-  }
-  function showStatus(error, stdout, sterr)
-  {
-    writePage();
+    response.write('<html><head><title>Picam Local Control</title></head>');
+    response.write('<body onload = "window.location.assign(window.location.protocol + ' + "'//'" + ' + window.location.hostname + ' + "':'" + ' + window.location.port)"/>');
+    response.end('</html>');
   }
 
   console.log("Got request: " + request.url);
@@ -34,10 +22,22 @@ function onRequest(request, response)
     case 'stop':
       // Start or Stop before displaying the updated state.
       command = "sudo /etc/init.d/pycam " + reqCommand;
-      exec(command, { timeout: 1000 }, showStatus);
+      exec(command, { timeout: 1000 }, reloadEntryPage);
       break;
     case '':
-      writePage();
+      // Display current status - 'Running' if pgrep returns non-empty string
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write('<html><head><title>Picam Local Control</title></head><body style="font-size:6vw;"><h1>Picam</h1><h1 style = "color:');
+    	exec("pgrep pycam.py", { timeout : 500 },
+    		function (error, stdout, sterr) {
+    			var running = (stdout != '');
+    			response.write(running ? 'green">Running' : 'red">Stopped');
+          response.write("</h1>");
+          // Display hyperlink to toggle on/off.
+          var command = (running ? 'Stop' : 'Start');
+          response.write('<h1><a href="/' + command + '">' + command + '</a></h1>');
+          response.end('</body></html>');
+    		});
       break;
     default:
       response.writeHead(404, {'Content-Type': 'text/plain'});
